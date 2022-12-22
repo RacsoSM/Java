e1f7a4bfc9cdaca35c147cf89b1785d52ca1d192
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-22 03:03:35 -0700

    Dado un array con numeros del 0-9 duplicar cada 0 encontrado en el array desplazando cada numero reemplazado una posicion hacia la derecha, sacando algunos numeros del array original

--- Logica/Problems/Arrays/Teory/insert/duplicate_zeros.java
+++ Logica/Problems/Arrays/Teory/insert/duplicate_zeros.java
@@ -1,6 +1,57 @@
 public class duplicate_zeros {
     
     public static void main(String[] args) {
+        /*Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+        Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+        
+        Example:1
+        Input: arr = [1,0,2,3,0,4,5,0]
+        Output: [1,0,0,2,3,0,0,4]
+        */
+
+
+        int arr [] = {1,0,2,3,0,4,5,0};
+        System.out.println("Input");
+        max_consecutive_ones.ImprimeInt(arr);
+
+        duplicateZeros(arr);
+
+        System.out.println("Output:");
+        max_consecutive_ones.ImprimeInt(arr);
+    }
+
+    public static void duplicateZeros(int[] arr) {
+        int x=0;
+        
+        for(int i=0;i<arr.length;i++){
+            
+             if(arr[i]==0){
+                 x++;
+             }
+        }
+        
+        int copia [] = new int [arr.length+x];
+        int t=0;
+        
+        for(int p=0;p<arr.length;p++){
+            
+            if(arr[p]==0){
+                copia[p+t]=0;
+                copia[p+1+t]=0;
+                t++;
+            }
+            else{
+                 copia[p+t]=arr[p];
+            }
+             
+        }
+        
+        
+        for(int y=0;y<arr.length;y++){
+        
+           arr[y]=copia[y];
+            
+        }
         
     }
 }
