c1c01473823ea34a7d95f01c5c5c14fb72cd29c6
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-22 01:09:01 -0700

    Indica como insertar al principio, final o donde sea en el array

--- Logica/Problems/Arrays/Teory/insert/insert.java
+++ Logica/Problems/Arrays/Teory/insert/insert.java
@@ -1,8 +1,63 @@
 public class insert {
     
     public static void main(String[] args) {
+        int arr [] = new int [6];
+        end(arr);
+        start(arr);
+        anywhere(arr);
 
-        
 
     }
+
+    public static void end (int [] arr){
+        //Insertar al final de un array
+        //Aqui llenamos las primeras 3 posiciones del array
+        int length=0;
+        for(int i=0; i<3;i++){ 
+            arr[length]=i;
+            length++;
+        }
+        //Hacemos un espacio adelante para el nuevo elemento 
+       
+
+        arr[length] = 10;
+        System.out.println("FIN DEL ARRAY");
+        max_consecutive_ones.ImprimeInt(arr);
+       
+
+    }
+
+    public static void start (int [] arr){
+        //Insertar al inicio de un array
+        //Hacemos un espacio adelante para el nuevo elemento 
+        for (int i = 3; i >= 0; i--) {
+           arr[i + 1] = arr[i];
+        }
+
+        arr[0] = 20;
+        System.out.println("\n\n\nINICIO DEL ARRAY");
+        max_consecutive_ones.ImprimeInt(arr);
+       
+
+    }
+
+    public static void anywhere (int [] arr){
+         //Donde sea en el array
+       
+
+         //Hacemos un espacio en el 2do espacio del array (numero cualquiera)
+         for (int i = 4; i >= 2; i--)
+          {
+            // Mandamos cada elemento un espacio a la derecha.
+             arr[i+1] = arr[i];
+          }
+ 
+         arr[2] = 30;
+ 
+         System.out.println("\n\n\nDONDE SEA DEL ARRAY");
+         max_consecutive_ones.ImprimeInt(arr);
+        
+ 
+     }
+    
 }
