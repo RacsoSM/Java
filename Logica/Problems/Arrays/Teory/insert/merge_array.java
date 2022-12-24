4a92ac374a2f5ab72c1ef649edb30fbc0ef775d6
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-23 21:35:18 -0700

    Agregando ejemplos y descripcion completa del problema

--- Logica/Problems/Arrays/Teory/insert/merge_array.java
+++ Logica/Problems/Arrays/Teory/insert/merge_array.java
@@ -3,7 +3,7 @@ public class merge_array {
 
     public static void main(String[] args) {
         
-        /*You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+       /*You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
         Merge nums1 and nums2 into a single array sorted in non-decreasing order.
         
         Example 1: 
