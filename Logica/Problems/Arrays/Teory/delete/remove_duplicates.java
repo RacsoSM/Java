ac9ceb4b765204c8bb5082b726f3631325d0e642
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-24 17:20:45 -0700

    Agregamos el metodo main para adaptar el problema

--- Logica/Problems/Arrays/Teory/delete/remove_duplicates.java
+++ Logica/Problems/Arrays/Teory/delete/remove_duplicates.java
@@ -2,9 +2,16 @@ public class remove_duplicates {
 
     public static void main(String[] args) {
         
+        int nums [] = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println("ARRAY ORIGINAL");
+        remove_element.ImprimeInt(nums);
+
+        removeDuplicates(nums);
+        System.out.println("ARRAY SIN DUPLICADOS");
+        remove_element.ImprimeInt(nums);
     }
 
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
         int copia [] = new int [nums.length];
         int t=0,x=0;
         
