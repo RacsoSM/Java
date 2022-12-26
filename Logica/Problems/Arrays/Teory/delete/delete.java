acc50e1c6c55188b9c61de72f3889d86e4a74a8f
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-25 18:20:12 -0700

    Creando metodo delete_first que elimina el primer elemento de un array

--- Logica/Problems/Arrays/Teory/delete/delete.java
+++ Logica/Problems/Arrays/Teory/delete/delete.java
@@ -16,17 +16,35 @@ public class delete {
         System.out.println("ARRAY ORIGINAL");
         remove_element.ImprimeInt(intArray);
 
-        System.out.println("\n\nARRAY ELIMINANDO ULTIMO ELEMENTO");
-        delete_first(intArray, length);
-      
-       
+      /*   System.out.println("\n\nARRAY ELIMINANDO ULTIMO ELEMENTO");
+        delete_last(intArray, length);
+      */
+
+      System.out.println("\n\nARRAY ELIMINANDO PRIMER ELEMENTO");
+      delete_first(intArray, length);
+      remove_element.ImprimeInt(intArray);
         
     } 
-    public static void delete_first (int [] intArray, int length){
+    public static void delete_last (int [] intArray, int length){
         length--;
         for(int i=0;i<length;i++){
             System.out.println("Index " + i + " contains " + intArray[i]);
         }
+    }
+
+    public static void delete_first (int [] intArray, int length){
+   
+       // Starting at index 1, we shift each element one position to the left.
+        for (int i = 1; i < length; i++) {
+            // Shift each element one position to the left
+            intArray[i - 1] = intArray[i];
+        }
+
+        // Note that it's important to reduce the length of the array by 1.
+        // Otherwise, we'll lose consistency of the size. This length
+        // variable is the only thing controlling where new elements might get added.
+     
+        length--;
     }
 
    
