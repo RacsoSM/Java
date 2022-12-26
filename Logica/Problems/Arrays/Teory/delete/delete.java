834989cab5a2fae41a7a5014fc940964a31c164d
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-25 18:10:00 -0700

    Creando cuerpo main declarando un array con el que se mostraran ejemplos

--- Logica/Problems/Arrays/Teory/delete/delete.java
+++ Logica/Problems/Arrays/Teory/delete/delete.java
@@ -1,6 +1,24 @@
 public class delete {
     
     public static void main(String[] args) {
+        // Declare an integer array of 10 elements.
+        int[] intArray = new int[10];
+
+        // The array currently contains 0 elements
+        int length=0;
+
+        // Add elements at the first 6 indexes of the Array.
+        for(int i = 0; i < 6; i++) {
+            intArray[length] = i;
+            length++;
+        }
+
+        System.out.println("ARRAY ORIGINAL");
+        remove_element.ImprimeInt(intArray);
         
     }
+
+   
+
+
 }
