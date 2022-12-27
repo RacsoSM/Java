8e21eaa9d3b17b2fb8d12350cf2b83d8a10e00f1
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-26 20:54:39 -0700

    Creando metodo main, definiendo el array y llamando al metodo

--- Logica/Problems/Arrays/Teory/search/n_double.java
+++ Logica/Problems/Arrays/Teory/search/n_double.java
@@ -2,9 +2,14 @@ public class n_double {
     
     public static void main(String[] args) {
         
+    
+        int arr [] = {10,2,5,3};
+        System.out.println("Se cumplen las caracteristicas requeridas?: "+checkIfExist(arr));
+
+
     }
 
-    public boolean checkIfExist(int[] arr) {
+    public static boolean checkIfExist(int[] arr) {
         
         for(int i=0;i<arr.length;i++){
              //   System.out.println("primer for, valor de i: "+i+ " valor del array en esta posición: "+arr[i]);
