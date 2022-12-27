6b805764823cdbe306ee45a479a7777b83085802
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-26 20:51:22 -0700

    Agregando el metodo checkIfExist que nos devuelve true si, dado el valor de la variable contadora de dos for anidados, en algun punto del array la variable del primer for es igual a la variable del segundo for multiplicado por dos, en caso contrario devuelve false

--- Logica/Problems/Arrays/Teory/search/n_double.java
+++ Logica/Problems/Arrays/Teory/search/n_double.java
@@ -3,4 +3,18 @@ public class n_double {
     public static void main(String[] args) {
         
     }
+
+    public boolean checkIfExist(int[] arr) {
+        
+        for(int i=0;i<arr.length;i++){
+             //   System.out.println("primer for, valor de i: "+i+ " valor del array en esta posición: "+arr[i]);
+            for(int j=0;j<arr.length;j++){
+             //   System.out.println("segundo for, valor de j: "+j+ " valor del array en esta posición: "+arr[j]);
+                if((arr[i]== (arr[j]*2)) && i!=j){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
