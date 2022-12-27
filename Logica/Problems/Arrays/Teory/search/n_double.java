b5546a2195515ac6314145fe4a30c80b985a12d7
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-26 20:56:19 -0700

    Agregando ejemplo con imput, output y explicación del resultado

--- Logica/Problems/Arrays/Teory/search/n_double.java
+++ Logica/Problems/Arrays/Teory/search/n_double.java
@@ -7,7 +7,10 @@ public class n_double {
         0 <= i, j < arr.length
         arr[i] == 2 * arr[j] 
         
-        
+        Example 1:
+        Input: arr = [10,2,5,3]
+        Output: true
+        Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
         */
 
         int arr [] = {10,2,5,3};
