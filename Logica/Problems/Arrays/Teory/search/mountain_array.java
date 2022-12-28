961064e14fcece02208015a4ce69ca047791bcd8
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-27 21:03:04 -0700

    Agregamos los requerimientos del problema

--- Logica/Problems/Arrays/Teory/search/mountain_array.java
+++ Logica/Problems/Arrays/Teory/search/mountain_array.java
@@ -2,10 +2,20 @@ public class mountain_array {
     
     public static void main(String[] args) {
 
+        /*
+          Given an array of integers arr, return true if and only if it is a valid mountain array.
 
+        Recall that arr is a mountain array if and only if:
+
+        arr.length >= 3
+        There exists some i with 0 < i < arr.length - 1 such that:
+        arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+        arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+         */
     int arr [] = {0,3,2,1};
 
     System.out.println("El array es un mountain array?: "+(validMountainArray(arr)));
+
     }
     public static boolean validMountainArray(int[] arr) {
         int count = 0;
