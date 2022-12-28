368fc8af4392a36ab27b9efd34d16871b1e34d32
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-27 21:04:06 -0700

    Agregamos ejemplos con su respectivo input y output

--- Logica/Problems/Arrays/Teory/search/mountain_array.java
+++ Logica/Problems/Arrays/Teory/search/mountain_array.java
@@ -11,6 +11,19 @@ public class mountain_array {
         There exists some i with 0 < i < arr.length - 1 such that:
         arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
         arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+
+        Example 1:
+        Input: arr = [2,1]
+        Output: false
+
+        Example 2:
+        Input: arr = [3,5,5]
+        Output: false
+
+        Example 3:
+        Input: arr = [0,3,2,1]
+        Output: true
          */
     int arr [] = {0,3,2,1};
 
