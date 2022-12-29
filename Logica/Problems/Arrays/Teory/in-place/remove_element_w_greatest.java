ad6f9a5393413d153ffeda6bb3f09981fa7cb72e
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-28 19:25:35 -0700

    Agregando instrucciones necesarias para resolver el problema correctamente

--- Logica/Problems/Arrays/Teory/in-place/remove_element_w_greatest.java
+++ Logica/Problems/Arrays/Teory/in-place/remove_element_w_greatest.java
@@ -1,7 +1,8 @@
 public class remove_element_w_greatest {
 
     public static void main(String[] args) {
-       
+        /*Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+        After doing so, return the array. */
 
         int arr [] = {17,18,5,4,6,1};
 
