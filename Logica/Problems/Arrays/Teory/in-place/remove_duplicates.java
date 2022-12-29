9174c8fc4e3838a4556e3d81bfa2b149ee8bdf49
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-29 01:01:39 -0700

    Agregando metodo main que define el array y manda a llamar al metodo

--- Logica/Problems/Arrays/Teory/in-place/remove_duplicates.java
+++ Logica/Problems/Arrays/Teory/in-place/remove_duplicates.java
@@ -1,23 +1,35 @@
 public class remove_duplicates {
 
     public static void main(String[] args) {
-        
+        int nums [] = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println("ARRAY ORIGINAL");
+        ImprimeInt(nums);
+
+        removeDuplicates(nums);
+
+        System.out.println("ARRAY CAMBIADO");
+        ImprimeInt(nums);
     }
 
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
         int k=0;
         
       for(int i=0;i<nums.length;i++){
           
           if(i==0||nums[i]!=nums[i-1]){
-              System.out.println(nums[i]);
               nums[k]=nums[i];
               k++;
           }
-          
-      }
-        
+      } 
         return k;
+    }
+
+    public static void ImprimeInt(int [] V){ 
+        for (int i = 0; i < V.length; i++) {
+            
+                System.out.println("["+V[i]+"]"+" Pos "+ i);
+            
+        }
     }
     
 }
