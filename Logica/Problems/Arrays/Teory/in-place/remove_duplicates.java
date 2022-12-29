c544d7bb3e213807ea1463b5eba55191192d303f
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-29 00:52:16 -0700

    Agregando metodo removeDuplicates que elimina los duplicados de un array pero de forma mas sencilla que la pasada, teniendo un menor nivel de complejidad menor

--- Logica/Problems/Arrays/Teory/in-place/remove_duplicates.java
+++ Logica/Problems/Arrays/Teory/in-place/remove_duplicates.java
@@ -3,5 +3,21 @@ public class remove_duplicates {
     public static void main(String[] args) {
         
     }
+
+    public int removeDuplicates(int[] nums) {
+        int k=0;
+        
+      for(int i=0;i<nums.length;i++){
+          
+          if(i==0||nums[i]!=nums[i-1]){
+              System.out.println(nums[i]);
+              nums[k]=nums[i];
+              k++;
+          }
+          
+      }
+        
+        return k;
+    }
     
 }
