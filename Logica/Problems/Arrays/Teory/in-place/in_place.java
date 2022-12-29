f6c915f9017fac2c0422b91be58d8c876060843a
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-28 19:46:39 -0700

    Creando metodo main y el metodo first, que eleva al cuadrado las posiciones pares del array, mientras que deja las impares con su mismo valor

--- Logica/Problems/Arrays/Teory/in-place/in_place.java
+++ Logica/Problems/Arrays/Teory/in-place/in_place.java
@@ -1,7 +1,49 @@
 public class in_place {
     
     public static void main(String[] args) {
-        
+        /*Given an Array of integers, return an Array where every element at an even-indexed position is squared.
+         
+         Input: array = [9, -2, -9, 11, 56, -12, -3]
+        Output: [81, -2, 81, 11, 3136, -12, 9]
+        Explanation: The numbers at even indexes (0, 2, 4, 6) have been squared, 
+        whereas the numbers at odd indexes (1, 3, 5) have been left the same.
+         
+        there are two ways we could approach it.
+        */
+        int array [] = {9,-2,-9,11,56,-12,-3};
+        int length = array.length;
+        System.out.println("ARRAY ORIGINAL");
+        remove_element_w_greatest.ImprimeInt(array);
+        first(array,length);
+
     }
+    public static int [] first (int [] array, int length){
+        // Check for edge cases.
+        if (array == null) {
+            return null;
+        }
+
+        // Create a resultant Array which would hold the result.
+        int result[] = new int[length];
+
+        // Iterate through the original Array.
+        for(int i = 0; i < length; i++) {
+
+            // Get the element from slot i of the input Array.
+            int element = array[i];
+
+            // If the index is an even number, then we need to square element.
+            if (i % 2 == 0) {
+            element *= element;
+            }
+
+            // Write element into the result Array.
+            result[i] = element;
+        }
+        System.out.println("\n\nARRAY CAMBIADO");
+        remove_element_w_greatest.ImprimeInt(result);
+        // Return the result Array.
+        return result;
+     }
     
 }
