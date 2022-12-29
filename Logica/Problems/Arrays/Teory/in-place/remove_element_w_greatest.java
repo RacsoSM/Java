c37469508dd9e20da6bafd9b8f09b72f52014c9f
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-28 19:20:02 -0700

    Agregando metodo main que define el array y manda a llamar al metodo

--- Logica/Problems/Arrays/Teory/in-place/remove_element_w_greatest.java
+++ Logica/Problems/Arrays/Teory/in-place/remove_element_w_greatest.java
@@ -1,7 +1,16 @@
 public class remove_element_w_greatest {
 
     public static void main(String[] args) {
-        
+       
+
+        int arr [] = {17,18,5,4,6,1};
+
+        System.out.println("ARRAY ORIGINAL");
+        ImprimeInt(arr);
+
+        System.out.println("ARRAY CAMBIADO");
+        replaceElements(arr);
+        ImprimeInt(arr);
     }
 
     public static int[] replaceElements(int[] arr) {
@@ -18,13 +27,21 @@ public class remove_element_w_greatest {
                 
             }
             arr[i]=x;
-            System.out.println(x);
+          //System.out.println(x);
             
             arr[i-1]=arr[i];
         }
            arr[arr.length-1]=-1;
         
         return arr;
+    }
+
+    public static void ImprimeInt(int [] V){ 
+        for (int i = 0; i < V.length; i++) {
+            
+                System.out.println("["+V[i]+"]"+" Pos "+ i);
+            
+        }
     }
     
 }
