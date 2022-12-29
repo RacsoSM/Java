36c3801dbaef0b1347c976fd6df9051d603ae2f8
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-28 19:50:03 -0700

    Agregamos metodo second que hace lo mismo que first pero de una forma mas simple, modificando nuestro mismo array en vez de crear uno nuevo, gastanto mas memoria, a esto se le llaman operaciones in-place

--- Logica/Problems/Arrays/Teory/in-place/in_place.java
+++ Logica/Problems/Arrays/Teory/in-place/in_place.java
@@ -14,7 +14,9 @@ public class in_place {
         int length = array.length;
         System.out.println("ARRAY ORIGINAL");
         remove_element_w_greatest.ImprimeInt(array);
-        first(array,length);
+     //   first(array,length);
+        second(array,length);
+
 
     }
     public static int [] first (int [] array, int length){
@@ -45,5 +47,30 @@ public class in_place {
         // Return the result Array.
         return result;
      }
+
+     public static int[] second(int[] array, int length) {
+
+        // Check for edge cases.
+        if (array == null) {
+          return array;
+        }
+      
+        // Iterate through the original array.
+        for(int i = 0; i < length; i++) {
+      
+          // If the index is an even number, then we need to square the element
+          // and replace the original value in the Array.
+          if (i % 2 == 0) {
+            array[i] *= array[i];
+          }
+          // Notice how we don't need to do *anything* for the odd indexes? :-)
+        }
+      
+        // We just return the original array. Some problems on leetcode require you
+        // to return it, and other's dont.
+        System.out.println("\n\nARRAY CAMBIADO");
+        remove_element_w_greatest.ImprimeInt(array);
+        return array;
+      }
     
 }
