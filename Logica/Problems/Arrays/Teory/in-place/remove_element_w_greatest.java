c77122f63810ff077fd20999eb4cb124ec82fbf2
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-28 19:15:57 -0700

    Agregando el metodo replaceElements que reemplaza el elemento de un array con el elemento mas grande que se encuentre a la derecha en todo el array (el mas grande del array sin contarse a si mismo)

--- Logica/Problems/Arrays/Teory/in-place/remove_element_w_greatest.java
+++ Logica/Problems/Arrays/Teory/in-place/remove_element_w_greatest.java
@@ -3,5 +3,28 @@ public class remove_element_w_greatest {
     public static void main(String[] args) {
         
     }
+
+    public static int[] replaceElements(int[] arr) {
+        int x=0;
+    
+        for(int i=1;i<arr.length;i++){
+            
+            x=0;
+            for(int y=i;y<arr.length;y++){
+                
+            if(arr[y]>x){
+                x=arr[y];
+            }
+                
+            }
+            arr[i]=x;
+            System.out.println(x);
+            
+            arr[i-1]=arr[i];
+        }
+           arr[arr.length-1]=-1;
+        
+        return arr;
+    }
     
 }
