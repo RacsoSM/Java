1b80022243b7f3c0ddaa160075e82aeb4535de9f
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2022-12-30 14:07:56 -0700

    Agregando instrucciones para resolver el problema

--- Logica/Problems/Arrays/Teory/in-place/move_zeroes.java
+++ Logica/Problems/Arrays/Teory/in-place/move_zeroes.java
@@ -1,7 +1,8 @@
 public class move_zeroes {
     
     public static void main(String[] args) {
-      
+        /*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+        Note that you must do this in-place without making a copy of the array. */
     }
     
 }
