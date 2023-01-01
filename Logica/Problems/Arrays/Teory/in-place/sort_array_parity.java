4ef7d9d5d333cca9d50baea0e9549400f1c70d9a
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-01 16:17:05 -0700

    Añadiendo las instrucciones para resolver el problema correctamente

--- Logica/Problems/Arrays/Teory/in-place/sort_array_parity.java
+++ Logica/Problems/Arrays/Teory/in-place/sort_array_parity.java
@@ -2,6 +2,10 @@ public class sort_array_parity {
 
     public static void main(String[] args) {
         
+        /*Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+        Return any array that satisfies this condition. */
+
+
     }
     
 }
