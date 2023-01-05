ed9ad732fccba648181fd84b363c37ab6d3764b7
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-04 22:55:13 -0700

    Añadiendo instrucciones para resolver correctamente el problema

--- Logica/Problems/Arrays/Teory/conclusion/third_maximum_number.java
+++ Logica/Problems/Arrays/Teory/conclusion/third_maximum_number.java
@@ -1,7 +1,7 @@
 public class third_maximum_number {
 
     public static void main(String[] args) {
-        
+        /*Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number. */
     }
     
 }
