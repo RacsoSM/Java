db4ca37fd262093c836de10ca81571cf09cbcbdc
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-05 20:51:30 -0700

    Agregando instrucciones para resolver correctamente el problema

--- Logica/Problems/Arrays/Teory/conclusion/numbers_missed.java
+++ Logica/Problems/Arrays/Teory/conclusion/numbers_missed.java
@@ -1,7 +1,9 @@
 public class numbers_missed {
     
     public static void main(String[] args) {
-        
+    
+        /*Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums. */
     }
+
     
 }
