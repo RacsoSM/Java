8e11c88e92d7fe71b8d929795fb107185c58ff55
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-07 22:05:39 -0700

    Añadiendo instrucciones para resolver el problema

--- Logica/Problems/Arrays/Teory/conclusion/largest_numbers_twice_other.java
+++ Logica/Problems/Arrays/Teory/conclusion/largest_numbers_twice_other.java
@@ -1,7 +1,8 @@
 public class largest_numbers_twice_others {
 
     public static void main(String[] args) {
-        
+        /*You are given an integer array nums where the largest integer is unique.
+        Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise. */
 
     }
 }
