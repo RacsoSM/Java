3b8601aef69d8f341f98dd7fd0d9568849223b3c
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-10 14:24:11 -0700

    Importando javax.swing y haciendo ejemplo de un string

--- Logica/Universidad/Pruebas/JOptionPane.java
+++ Logica/Universidad/Pruebas/JOptionPane.java
@@ -1,6 +1,15 @@
+import javax.swing.JOptionPane; //NECESARIO IMPORTAR PARA USAR ESTA CLASE
 public class JOptionPane {
     
     public static void main(String[] args) {
         
+        //ES PARA QUE LA ENTRADA Y SALIDA DE DATOS NO SEA POR CONSOLA SI NO UN POCO MAS GRAFICA
+
+        String cadena; 
+        int entero; 
+        char letra; 
+        double decimal;
+
+        cadena = javax.swing.JOptionPane.showInputDialog( "Digite una cadena: ");
     }
 }
