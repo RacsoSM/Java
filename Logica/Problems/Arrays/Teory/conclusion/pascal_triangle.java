5b309fb36d659900fa22c47137e89080ff877b38
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-09 20:42:32 -0700

    Agregando instrucciones para resolver correctamente el problema

--- Logica/Problems/Arrays/Teory/conclusion/pascal_triangle.java
+++ Logica/Problems/Arrays/Teory/conclusion/pascal_triangle.java
@@ -1,6 +1,7 @@
 public class pascal_triangle {
     public static void main(String[] args) {
-        System.out.println("hola");
+       /*Given an integer numRows, return the first numRows of Pascal's triangle.
+        In Pascal's triangle, each number is the sum of the two numbers directly above it as shown: */
     }
     
 }
