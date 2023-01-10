389956c0e32ecfeaab691e05c9b29f1b6f51d5bf
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-10 13:33:53 -0700

    Cambios en la estructura de las carpetas del repositorio

---
+++ Logica/Problems/ArrayList/Problems/pascal_triangle.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class pascal_triangle {
+    public static void main(String[] args) {
+       /*Given an integer numRows, return the first numRows of Pascal's triangle.
+        In Pascal's triangle, each number is the sum of the two numbers directly above it as shown: 
+        
+                    1
+                   1 1
+                  1 2 1
+                 1 3 3 1
+                1 4 6 4 1
+
+        Example 1:
+        Input: numRows = 5
+        Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+        Example 2:
+        Input: numRows = 1
+        Output: [[1]]
+
+        Constraints:
+        1 <= numRows <= 30
+        */
+        int numRows = 9;
+        generate(numRows);
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        if(numRows == 0) return new ArrayList();
+         List<List<Integer>> res = new ArrayList();
+ 
+         for(int i = 1; i <= numRows; i++){
+             List<Integer> row = new ArrayList();
+             for(int j = 0 ; j < i ; j++){
+                 if(j == 0  || j== i-1){
+                     row.add(1);
+                 }else{
+                     row.add(res.get(i-2).get(j) + res.get(i-2).get(j-1));
+                 } 
+                 System.out.print("["+row.get(j)+"]");      
+             }
+             System.out.println();
+           
+             res.add(row);
+         }
+
+        
+         return res;
+     }
+
+    
+
+
+    
+}
