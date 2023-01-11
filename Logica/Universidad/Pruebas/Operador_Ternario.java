af0f9ffbaf9c89ef584ae808977193b6438e5d2d
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-10 17:17:40 -0700

    Mostrando resultado y explicando ejemplo

--- Logica/Universidad/Pruebas/Operador_Ternario.java
+++ Logica/Universidad/Pruebas/Operador_Ternario.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Operador_Ternario {
     public static void main(String[] args) {
-        System.out.println("hola");
-    }
+        
+        //este operador es un if else mas simplificado, si el valor de la condición
+        //es verdadero, toma el valor 1, en caso contrario, toma el valor 2.
+
+
+        //SINTAXIS:
+        //valor = (condicion) ? valor1 : valor 2;
+
+
+        //Usar operador ternario para comprobar si un numero es par o no.
+
+        Scanner sc = new Scanner (System.in);
+        int numero=0;
+        String mensaje ="";
+
+        System.out.println("Ingrese un numero: "); numero=sc.nextInt();
+
+        mensaje = (numero%2==0) ? "Es par" : "Es impar";
+        //Si la condicion es true, toma el primer valor y si es false, toma el segundo valor
+
+        System.out.println(mensaje);
+    }   
     
 }
