3afe54a8b03354828d46038ddc131f52ea521404
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-11 15:56:51 -0700

    Agregando explicacion y sintaxis

--- Logica/Universidad/Pruebas/try_catch.java
+++ Logica/Universidad/Pruebas/try_catch.java
@@ -1,7 +1,16 @@
 public class try_catch {
     public static void main(String[] args) {
         
-        System.out.println("hola");
+        /*
+         El metodo try catch sirve para capturar problemas del lenguaje, se utiliza de la siguiente forma:
+         try{
+            //sentencias si el programa se ejecuta correctamente
+         }catch(tipo de problema a capturar){
+            //sentencias si se encuentra el error
+         }
+
+         en caso de no encontrarse problemas o errores, solo se ejecutará el metodo try, si se encuentra el problema se ejecutará el metodo catch.
+         */
 
     }
 }
