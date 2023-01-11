c29afaf02d4fb697c5cc7ea76f4511d2230c0dae
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-10 17:08:05 -0700

    Documentando que hace cada linea

--- Logica/Universidad/Pruebas/JOptionPane.java
+++ Logica/Universidad/Pruebas/JOptionPane.java
@@ -10,14 +10,14 @@ public class JOptionPane {
         char letra; 
         double decimal;
 
-        cadena = javax.swing.JOptionPane.showInputDialog( "Digite una cadena: ");
-        entero= Integer.parseInt(javax.swing.JOptionPane.showInputDialog( "Digite un entero: "));
-        letra = javax.swing.JOptionPane.showInputDialog("Digite un caracter: ").charAt(0);
-        decimal= Double.parseDouble(javax.swing.JOptionPane.showInputDialog( "Digite un decimal: "));
+        cadena = javax.swing.JOptionPane.showInputDialog( "Digite una cadena: "); //LEER UN STRING
+        entero= Integer.parseInt(javax.swing.JOptionPane.showInputDialog( "Digite un entero: ")); //LEER UN ENTERO
+        letra = javax.swing.JOptionPane.showInputDialog("Digite un caracter: ").charAt(0); //LEER UN CHAR
+        decimal= Double.parseDouble(javax.swing.JOptionPane.showInputDialog( "Digite un decimal: ")); //LEER UN DOUBLE
 
-        javax.swing.JOptionPane.showMessageDialog(null, "La cadena es "+cadena);
-        javax.swing.JOptionPane.showMessageDialog(null, "El entero es "+entero);
-        javax.swing.JOptionPane.showMessageDialog(null, "El caracter es "+letra);
-        javax.swing.JOptionPane.showMessageDialog(null, "El numero decimal es "+decimal);
+        javax.swing.JOptionPane.showMessageDialog(null, "La cadena es "+cadena); //MOSTRAR UN STRING
+        javax.swing.JOptionPane.showMessageDialog(null, "El entero es "+entero); //MOSTRAR UN ENTERO
+        javax.swing.JOptionPane.showMessageDialog(null, "El caracter es "+letra); //MOSTRAR UN CHAR
+        javax.swing.JOptionPane.showMessageDialog(null, "El numero decimal es "+decimal); //MOSTRAR UN DOUBLE
     }
 }
