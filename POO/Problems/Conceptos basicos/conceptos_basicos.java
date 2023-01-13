5867808b21b7b097a57429ada5dc0a7a8ab709fc
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-12 23:12:01 -0700

    Explicando atributos y metodos

--- POO/Problems/Conceptos basicos/conceptos_basicos.java
+++ POO/Problems/Conceptos basicos/conceptos_basicos.java
@@ -1,5 +1,8 @@
 public class conceptos_basicos {
     public static void main(String[] args) {
+        /*Un objeto se denomina así por 2 caracteristicas principales, tener atributos (caracteristicas) y metodos (acciones)
+         * Un auto por ejemplo, seria un objeto por que sus atributos pueden ser : color, marca y kilometraje, y sus metodos pueden ser: encender, acelerar y frenar.
+        */
         
     }
     
