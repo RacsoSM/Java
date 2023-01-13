29cee96dad2c2c9f87bbf10e8082663a1d218ec7
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-12 23:15:17 -0700

    Explicando las clases

--- POO/Problems/Conceptos basicos/conceptos_basicos.java
+++ POO/Problems/Conceptos basicos/conceptos_basicos.java
@@ -2,6 +2,17 @@ public class conceptos_basicos {
     public static void main(String[] args) {
         /*Un objeto se denomina así por 2 caracteristicas principales, tener atributos (caracteristicas) y metodos (acciones)
          * Un auto por ejemplo, seria un objeto por que sus atributos pueden ser : color, marca y kilometraje, y sus metodos pueden ser: encender, acelerar y frenar.
+         * 
+         * Clases: los nombres de las clases deben ser en singular y siempre empezando por mayuscula.
+         * Una clase es un conjunto de objetos con caracteristicas similares:
+         * 
+         * Clase : Auto
+         * -color
+         * -marca
+         * -km
+         * encender()
+         * acelerar()
+         * frenar()
         */
         
     }
