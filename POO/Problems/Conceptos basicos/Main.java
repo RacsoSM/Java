fc41d4336c0d6b96e5ca3f9b4fcf5292424eb212
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-13 23:47:52 -0700

    Mostrando resultados en el main con los metodos

--- POO/Problems/Conceptos basicos/Main.java
+++ POO/Problems/Conceptos basicos/Main.java
@@ -1,6 +1,14 @@
+//ESTE MAIN ES EL EJECUTABLE DEL ARCHIVO "Operacion"
 public class Main {
     public static void main(String[] args) {
         Operacion op = new Operacion(); //Creando objeto de la clase operacion
+
+        op.leerNumeros();
+        op.sumar();
+        op.restar();
+        op.multiplicar();
+        op.dividir();
+        op.mostrarResultados();
 
 
     }
--- POO/Problems/Conceptos basicos/Operacion.java
+++ POO/Problems/Conceptos basicos/Operacion.java
@@ -9,29 +9,30 @@ public class Operacion {
 
     //Metodo para pedirle al usuario que nos digite 2 numeros
     public void leerNumeros(){
-        numero1=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
-        numero1=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        num1=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        num2=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
     }
 
     //Metodo para sumar ambos numeros
     public void sumar(){
-        suma = numero1+numero2;
+        suma = num1+num2;
     }
 
     //Metodo para restar ambos numeros
     public void restar(){
-        resta = numero1-numero2;
+        resta = num1-num2;
     }
 
     //Metodo para multiplicar ambos numeros
     public void multiplicar(){
-        multiplicacion = numero1*numero2;
+        multiplicacion = num1*num2;
     }
 
     
     //Metodo para dividir ambos numeros
     public void dividir(){
-    division = numero1/numero2;
+        
+    division =  num1/num2 ;
     }
 
     //Metodo para mostrar resultados
