48c36fcc8d95125c5190e58f9708d4118c07ec48
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-13 23:27:39 -0700

    Agregando metodo leerNumeros

--- POO/Problems/Conceptos basicos/Operacion.java
+++ POO/Problems/Conceptos basicos/Operacion.java
@@ -1,10 +1,18 @@
+import javax.swing.JOptionPane;
+
 public class Operacion {
- //Atributos
-    int num1,num2,suma,resta,multiplicacion,division;
+    //Atributos
+        int num1,num2,suma,resta,multiplicacion,division;
 
 
- //Metodos
-    
+    //Metodos
+
+    //Metodo para pedirle al usuario que nos digite 2 numeros
+    public void leerNumeros(){
+        numero1=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        numero1=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+
+}
 
 
 
