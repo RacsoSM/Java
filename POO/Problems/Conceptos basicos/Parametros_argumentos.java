742b27ecbe96cfe91c217fecedab7c58b280702c
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-14 23:22:52 -0700

    Agregando conceptos de declaracion e invocacion de metodo

--- POO/Problems/Conceptos basicos/Parametros_argumentos.java
+++ POO/Problems/Conceptos basicos/Parametros_argumentos.java
@@ -1,4 +1,14 @@
 public class Parametros_argumentos {
     //Parametros: es una declaracion de variable u objeto
     //Argumentos: es un valor que se envia
+
+
+    /*Declaracion de metodo:  
+     public void metodo (){
+        //lineas de codigo
+    } 
+    
+    //Invocacion de metodo:
+    objeto.metodo();
+    */
 }
