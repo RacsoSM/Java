915310256b3b4e948deab93d75fb2ef63be13e85
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-14 23:35:33 -0700

    Pedimos los numeros en el main del archivo Parametros_argumentos y creamos el objeto

--- POO/Problems/Conceptos basicos/Parametros_argumentos.java
+++ POO/Problems/Conceptos basicos/Parametros_argumentos.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class Parametros_argumentos {
     //Parametros: es una declaracion de variable u objeto
     //Argumentos: es un valor que se envia
@@ -9,9 +11,16 @@ public class Parametros_argumentos {
     } 
     //Invocacion de metodo:
     objeto.metodo(1,"Hola"); //ARGUMENTO
-
-
-
-
     */
+
+
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+
+        Operacion op = new Operacion();
+
+       
+    }
 }
+
