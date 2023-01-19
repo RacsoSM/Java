7eaaf5a07305f3f242202bf81d138bf9011d3d69
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-18 23:19:27 -0700

    Creando constructor Persona y metodo mostrarDatos

--- POO/Problems/Conceptos basicos/Persona.java
+++ POO/Problems/Conceptos basicos/Persona.java
@@ -1,3 +1,19 @@
 public class Persona {
-    
+    //Atributos
+    String nombre;
+    int edad;
+
+    //Metodos
+
+    //Metodo constructor
+    /*Este metodo nos sirve para inicializar nuestros atributos y debemos darle como parametros los atributos y dentro del mismo inicializarlos como se ve a continuacion. */
+   public Persona(String _nombre, int _edad){ 
+    nombre = _nombre; //
+    edad = _edad;
+   }
+
+   public void mostrarDatos(){
+    System.out.println("El nombre es: "+nombre);
+    System.out.println("La edad es: "+edad);
+   }
 }
