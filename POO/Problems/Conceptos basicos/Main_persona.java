a276b4ebc19c821ad115e40092c835112d69d9c8
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-18 23:14:35 -0700

    Teoria de los constructores

--- POO/Problems/Conceptos basicos/Main_persona.java
+++ POO/Problems/Conceptos basicos/Main_persona.java
@@ -1,3 +1,18 @@
 public class Main_persona {
     
+    /*Cuando se crea un objeto ocurren 3 cosas: 
+     * Se asigna memoria para el objeto.
+     * Se inicializan los atributos de ese objeto.
+     * Se invoca al constructor de la clase que puede ser uno entre varios.
+     * 
+     * Persona p1 = new Persona();
+     *                  Contructor = Persona()
+     * 
+     * Caracteristicas de los contructores:
+     * Tienen el mismo nombre de la clase
+     * No devuelve ningun valor
+     * Debe declararse como publico
+     */
+
+
 }
