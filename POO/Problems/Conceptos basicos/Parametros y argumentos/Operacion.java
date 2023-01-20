5c81a7c613ca024a94fb08069523b9faffc60137
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-19 22:50:37 -0700

    Creando carpeta Parametros y argumentos

---
+++ POO/Problems/Conceptos basicos/Parametros y argumentos/Operacion.java
@@ -0,0 +1,50 @@
+import javax.swing.JOptionPane;
+
+public class Operacion {
+
+
+    //Metodos
+
+    //Metodo para pedirle al usuario que nos digite 2 numeros
+    public void leerNumeros(){
+        num1=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        num2=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+    }
+
+    //Metodo para sumar ambos numeros
+    public int sumar(int num1, int num2){ //Ya estamos usando parametros
+       int suma = num1+num2;
+       return suma;
+    }
+
+    //Metodo para restar ambos numeros
+    public int restar(int num1, int num2){
+        int resta = num1-num2;
+        return resta;
+    }
+
+    //Metodo para multiplicar ambos numeros
+    public int multiplicar(int num1, int num2){
+        int multiplicacion = num1*num2;
+        return multiplicacion;
+    }
+
+    
+    //Metodo para dividir ambos numeros
+    public int dividir(int num1, int num2){
+        
+    int division =  num1/num2 ;
+    return division;
+    }
+
+    //Metodo para mostrar resultados
+    public void mostrarResultados(int suma, int resta, int multiplicacion, int division){
+        System.out.println("La suma es: "+suma);
+        System.out.println("La resta es: "+resta);
+        System.out.println("La multiplicacion es: "+multiplicacion);
+        System.out.println("La division es: "+division);
+    }
+
+
+    
+}
