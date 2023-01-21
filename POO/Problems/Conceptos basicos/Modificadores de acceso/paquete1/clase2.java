4224e5081ff5833294143b2c1799290e0f0d12e1
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-20 22:34:19 -0700

    Creando un objeto de la clase1 en la clase2 y dandole un valor al atributo, luego lo imprimimos

--- POO/Problems/Conceptos basicos/Modificadores de acceso/paquete1/clase2.java
+++ POO/Problems/Conceptos basicos/Modificadores de acceso/paquete1/clase2.java
@@ -2,6 +2,13 @@ public class clase2 {
     
     public static void main(String[] args) {
         
+        clase1 objeto1 = new clase1();
+
+        objeto1.atributo1 = 15;
+
+        System.out.println(objeto1.atributo1);
+
+
     }
     
 }
