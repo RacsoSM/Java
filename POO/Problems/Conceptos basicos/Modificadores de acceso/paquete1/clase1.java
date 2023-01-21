c64db972eb38a6f26ad8fa397c204b563d33b08b
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-20 22:39:35 -0700

    Agregando explicacion de public y private

--- POO/Problems/Conceptos basicos/Modificadores de acceso/paquete1/clase1.java
+++ POO/Problems/Conceptos basicos/Modificadores de acceso/paquete1/clase1.java
@@ -1,7 +1,12 @@
 public class clase1 {
 
 
-   private int atributo1;
+   /*private da error en clase2 y clase3*/public int atributo1;
 
+
+  /*Los modificadores de acceso tienen dos valores, public y private.
+   * public: nos dice que el atributo, clase o metodo puede ser usado en las demas clases y puede ser importada y usada en clases de otros paquetes.
+   * private: nos dice que el atributo, clase o metodo NO puede ser usado en las demas clases, solo podrá ser usado en la clase en la que fue declarado, en este caso clase1
+   */
     
 }
