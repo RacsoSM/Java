dbb1b9060e289e48617f3749932970e9b248eb37
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-20 22:36:24 -0700

    Importando clase1 y creando otro objeto, mostrando su valor

--- POO/Problems/Conceptos basicos/Modificadores de acceso/paquete2/clase3.java
+++ POO/Problems/Conceptos basicos/Modificadores de acceso/paquete2/clase3.java
@@ -1,8 +1,12 @@
-
-
+import paquete1.clase1;
 public class clase3 {
     public static void main(String[] args) {
-        
+       
+        clase1 objeto2 = new clase1();
+
+        objeto2.atributo1=10;
+
+        System.out.println(objeto2.atributo1);
     }
-  
+
 }
