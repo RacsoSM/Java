b8c3728a518bceff8f2bba42817d2a4f11a1499a
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-22 14:38:48 -0700

    Explicando propiedades del static

--- POO/Problems/Conceptos basicos/Metodos estaticos/Estatico.java
+++ POO/Problems/Conceptos basicos/Metodos estaticos/Estatico.java
@@ -1,6 +1,13 @@
 public class Estatico {
 
-    private String nombre = "Primera frase";
+    //La palabra static hace que los atributos o metodos no pertenezcan a los objetos si no a la clase, por lo que ya no podriamos llamarlos mediante un objeto pero si mediante el mismo nombre
+    //de la clase, ejemplo "Estatico.frase;"
+    //Esto causa que cualquier cambio que le hagamos al atributo mediante el objeto, todos los objetos tomen ese valor en ese atributo
+    private static String frase = "Primera frase";
+
+
+
+  
 
 
 
@@ -8,9 +15,15 @@ public class Estatico {
    public static void main(String[] args) {
 
     Estatico ob1 = new Estatico();
+    Estatico ob2 = new Estatico();
 
-    System.out.println(ob1.nombre);
 
+    ob2.frase="Segunda frase";
+
+   
+
+    System.out.println(ob1.frase);
+    System.out.println(ob2.frase);
    }
     
 }
