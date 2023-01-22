32e529f7e996b2abcb089e05f7de555bac6a4bf8
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-21 22:44:08 -0700

    Creando atributo private nombre y setter de nombre

--- POO/Problems/Conceptos basicos/Encapsulamiento y metodos accesores/c1.java
+++ POO/Problems/Conceptos basicos/Encapsulamiento y metodos accesores/c1.java
@@ -1,7 +1,7 @@
 public class c1 {
     
     private int edad;
-   
+    private String nombre;
 
     public void setEdad(int edad){
         this.edad=edad;
@@ -10,5 +10,11 @@ public class c1 {
     public int getEdad(){
         return edad;
     }
+
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+   
 
 }
