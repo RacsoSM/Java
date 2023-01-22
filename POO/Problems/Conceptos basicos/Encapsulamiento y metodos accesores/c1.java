08186e2268a70b80f9e931f8a867381f8fbce805
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-21 22:58:54 -0700

    Añadiendo documentacion

--- POO/Problems/Conceptos basicos/Encapsulamiento y metodos accesores/c1.java
+++ POO/Problems/Conceptos basicos/Encapsulamiento y metodos accesores/c1.java
@@ -2,11 +2,17 @@ public class c1 {
     
     private int edad;
     private String nombre;
+    /*El encapsulamiento es, cuando tenemos un atributo private poderlo modificar mediante estos metodos accesores para que no se puedan modificar por ninguna otra clase que no sean
+     * las mismas de la clase original.
+     */
 
+
+    //Metodo setter: establecemos la edad.
     public void setEdad(int edad){
         this.edad=edad;
     }
 
+    //Metodo getter: mostramos la edad.
     public int getEdad(){
         return edad;
     }
