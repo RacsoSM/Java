809bb3729b8c45efb8d01f931ebe216683098fbd
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-22 14:45:34 -0700

    Haciendo documentacion

--- POO/Problems/Conceptos basicos/Metodos estaticos/Estatico.java
+++ POO/Problems/Conceptos basicos/Metodos estaticos/Estatico.java
@@ -6,6 +6,12 @@ public class Estatico {
     private static String frase = "Primera frase";
 
 
+    //final es una palabra que hace que un atributo se vuelva constante. 
+    /* private final String nombre;
+     * esa linea hace que despues de que le demos un valor a nombre, no se pueda cambiar por lo que se vuelve una constante y no una variable, no lo podremos cambiar ni con un metodo setter
+     */
+
+    //Como este metodo es static, lo tendremos que llamar de la siguiente manera: Estatico.sumarNumeros(parametro1,parametro2);
     public static int sumarNumeros (int n1,int n2){
         int suma = n1+n2;
         return suma;
@@ -15,13 +21,17 @@ public class Estatico {
 
    public static void main(String[] args) {
 
+    //Creamos 2 objetos de tipo Estatico
     Estatico ob1 = new Estatico();
     Estatico ob2 = new Estatico();
 
+    //Llamamos al metodo sumarNumeros
     System.out.println("La suma es: "+Estatico.sumarNumeros(3, 4));
 
+    //Mostramos la frase desde la clase, ya que ahora le pertenece a esta y no a los objetos por el static
     System.out.println(Estatico.frase);
 
+    //Al cambiar en cualquier objeto el atributo frase, todos los objetos lo tomaran como su valor
     ob2.frase="Segunda frase";
 
     System.out.println(ob1.frase);
