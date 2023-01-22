d401d5cce3ff596ac3fecc193b58f6366e908941
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-21 22:41:35 -0700

    Creando objeto de tipo c1 y asi mostrar y modificar el atributo edad a travez del metodo setter, lo mostramos con el getter

--- POO/Problems/Conceptos basicos/Encapsulamiento y metodos accesores/c2.java
+++ POO/Problems/Conceptos basicos/Encapsulamiento y metodos accesores/c2.java
@@ -1,5 +1,10 @@
 public class c2 {
     public static void main(String[] args) {
       
+        c1 ob = new c1();
+
+        ob.setEdad(18);
+
+        System.out.println("La edad es: "+ob.getEdad());
     }
 }
