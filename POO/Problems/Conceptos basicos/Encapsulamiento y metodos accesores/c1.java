c2bcbef1aae779208be6837a778254e83e298295
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-21 22:40:20 -0700

    Creando metodo getter

--- POO/Problems/Conceptos basicos/Encapsulamiento y metodos accesores/c1.java
+++ POO/Problems/Conceptos basicos/Encapsulamiento y metodos accesores/c1.java
@@ -2,8 +2,13 @@ public class c1 {
     
     private int edad;
    
+
     public void setEdad(int edad){
         this.edad=edad;
+    }
+
+    public int getEdad(){
+        return edad;
     }
 
 }
