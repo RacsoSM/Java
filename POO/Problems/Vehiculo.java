f67c9fed79897a51cf5a65f37c391573e4522840
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-23 23:12:49 -0700

    Creamos metodo constructor

--- POO/Problems/Vehiculo.java
+++ POO/Problems/Vehiculo.java
@@ -3,8 +3,17 @@
  * de cada vehiculo y crear una clase que represente a cada uno de ellos
  */
 public class Vehiculo {
+    //Atributos
     private String marca;
     private String modelo;
     private float precio;
+
+    
+    //Constructor
+    public Vehiculo(String marca, String modelo, float precio){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
     
 }
