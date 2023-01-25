23eb0351765dacba46dce679cc975eeba5baae50
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-25 01:04:30 -0700

    Añadiendo instrucciones para resolver el problema

--- POO/Problems/Triangulo_isoceles.java
+++ POO/Problems/Triangulo_isoceles.java
@@ -1,3 +1,8 @@
+/*Diseñar un programa para trabajar con triangulos isosceles. Para ello defina los atributos necesarios que se requieren
+ * , proporcione metodos de consulta, un metodo constructor e implemente metodos para calcular el perimetro y el area de un 
+ * triangulo ademas implementar un metodo que a partir de un arreglo de triangulos devuelva el area del triangulo de mayor 
+ * superficie.
+ */
 
 public class Triangulo_isoceles {
   
