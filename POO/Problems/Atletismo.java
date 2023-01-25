fdab48b5c4a4ff60ad2720d4db3ab6f6ffa3d3f8
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-24 22:02:19 -0700

    Agregando instrucciones del ejercicio

--- POO/Problems/Atletismo.java
+++ POO/Problems/Atletismo.java
@@ -1,3 +1,7 @@
+/*Construir un programa para una competencia de atletismo, el programa debe gestionar una serie de atletas
+ * caracterizados por su numero de atleta, nombre y tiempo de carrera, al final el programa debe mostrar los 
+ * datos del atleta ganador de la carrera
+*/
 public class Atletismo {
     
 }
