a6a52164415391cfdbca16348599f88d7156a7cd
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-26 23:23:56 -0700

    Metodo mostrarDatos

--- POO/Problems/Conceptos basicos/Herencia/Estudiante.java
+++ POO/Problems/Conceptos basicos/Herencia/Estudiante.java
@@ -4,10 +4,23 @@ public class Estudiante extends Persona{//Con extends y el nombre de la clase le
    private int codigoEstudiante;
    private float notaFinal;
 
+
+   //Constructor de una clase hija, ponemos de parametros los atributos que heredó y tambien los propios
+   //Los heredados no los debemos inicializar de nuevo, por lo cual ponemos super y dichos atributos
     public Estudiante(String nombre, String apellido, int edad, int codigoEstudiante,float notaFinal){
         super(nombre, apellido, edad); //Con esta linea le decimos a java que nombre, apellido y edad ya estan inicializados dentro de Persona
         this.codigoEstudiante = codigoEstudiante;
         this.notaFinal = notaFinal;
+    }
+
+    //Mostrando todos los datos del estudiante
+    public void mostrarDatos(){
+        System.out.println("Mostrando los datos del estudiante: ");
+        System.out.println("Nombre: "+getNombre()+  //se usan los getters por que los atributos estan encapsulados
+            "\nApellido: "+getApellido()+
+            "\nEdad: "+getEdad()+
+            "\nCodigo de estudainte: "+codigoEstudiante+    //estos atributos ya no estan encapsulados
+            "\nNota final: "+notaFinal);
     }
     
 }
