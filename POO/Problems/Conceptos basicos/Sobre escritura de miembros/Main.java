66555f1eeb3b43e57e75726d9d0d2658a5332f7a
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-27 15:14:43 -0700

    Mostramos resultados en el main

--- POO/Problems/Conceptos basicos/Sobre escritura de miembros/Main.java
+++ POO/Problems/Conceptos basicos/Sobre escritura de miembros/Main.java
@@ -2,6 +2,12 @@ public class Main {
     //@Override le indica a java que estamos sobre escribiendo un metodo o atributo heredado
 
       public static void main(String[] args) {
-        
+        Persona p1 = new Persona(); //Creamos los objetos de cada clase y llamamos a los metodos
+        Perro d1 = new Perro();
+        Vaca v1 = new Vaca();
+
+        p1.comer();
+        d1.comer();
+        v1.comer();
       }
 }
