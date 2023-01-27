7a43b7d5fc2fde7a48b7b7c6c0f4b66b48c36106
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-26 23:08:50 -0700

    Creando los metodos de nuestra clase Persona

--- POO/Problems/Conceptos basicos/Herencia/Persona.java
+++ POO/Problems/Conceptos basicos/Herencia/Persona.java
@@ -1,6 +1,31 @@
 public class Persona {
+    //Atributos
     private String nombre;
     private String apellido;
     private int edad;
+
+    //Metodos
+    //Constructor
+    Persona(String nombre, String apellido, int edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    //getter de nombre
+    public String getNombre (){
+        return nombre;
+    }
+
+    //getter de apellido 
+    public String getApellido (){
+        return apellido;
+    }
+
+    //getter de edad
+    public int getEdad (){
+        return edad;
+    }
+
 }
 
