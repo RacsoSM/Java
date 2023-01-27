23dc1badb6258e86dd4f1c9c0d7b6443c8ed0435
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-26 23:12:46 -0700

    Documentacion

--- POO/Problems/Conceptos basicos/Herencia/Estudiante.java
+++ POO/Problems/Conceptos basicos/Herencia/Estudiante.java
@@ -1,4 +1,9 @@
-public class Estudiante {
-   
+public class Estudiante extends Persona{//Con extends y el nombre de la clase le decimos a java que estudiante hereda todo de persona
+    
+    //Agregamos los atributos unicos de estudiante que persona NO tiene
+   private int codigoEstudiante;
+   private float notaFinal;
+
+
     
 }
