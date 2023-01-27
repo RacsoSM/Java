fdecce1bad09020d6746b708c71b210c617483e3
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-26 23:10:24 -0700

    Documentacion en main

--- POO/Problems/Conceptos basicos/Herencia/Main.java
+++ POO/Problems/Conceptos basicos/Herencia/Main.java
@@ -1,3 +1,8 @@
 public class Main {
    
 }
+/*La herencia es como su nombre lo indica, heredar atributos y metodos de una clase ya hecha en una nueva, esto
+ * para ahorrarnos codigo
+ * 
+ * extends es la palabra con la que le indicamos a java que una clase es hija de otra
+ */
