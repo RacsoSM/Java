fcdd1d293f7ab035ef15fe70b02c9253e741ee6f
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-27 15:13:43 -0700

    Agregamos el main a la clase main

--- POO/Problems/Conceptos basicos/Sobre escritura de miembros/Main.java
+++ POO/Problems/Conceptos basicos/Sobre escritura de miembros/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     //@Override le indica a java que estamos sobre escribiendo un metodo o atributo heredado
 
-      
+      public static void main(String[] args) {
+        
+      }
 }
