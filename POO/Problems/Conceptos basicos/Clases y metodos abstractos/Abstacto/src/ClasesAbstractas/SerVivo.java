3bfd4a62dc2d97b613b6fd87b1c9e1d64244dbc5
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-28 19:41:28 -0700

    Creando clase abstracta SerVivo y metodo abstracto alimentarse

--- POO/Problems/Conceptos basicos/Clases y metodos abstractos/Abstacto/src/ClasesAbstractas/SerVivo.java
+++ POO/Problems/Conceptos basicos/Clases y metodos abstractos/Abstacto/src/ClasesAbstractas/SerVivo.java
@@ -1,7 +1,9 @@
 package ClasesAbstractas;
 
-public abstract class SerVivo {
-    
-    public abstract void alimentarse();
+//Como esta es la primera clase padre, la de hasta arriba de la jerarquia, se le pone abstract despues del modificador de acceso
+public abstract class SerVivo { 
+
+    //En las clases tambien se pone este abstract
+    public abstract void alimentarse(); 
 
 }
