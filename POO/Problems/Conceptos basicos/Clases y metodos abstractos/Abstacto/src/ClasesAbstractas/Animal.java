dc11f173a943fdea734ddbce702c1cf827e52c90
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-28 19:50:26 -0700

    Haciendo herencia de Animal

--- POO/Problems/Conceptos basicos/Clases y metodos abstractos/Abstacto/src/ClasesAbstractas/Animal.java
+++ POO/Problems/Conceptos basicos/Clases y metodos abstractos/Abstacto/src/ClasesAbstractas/Animal.java
@@ -2,6 +2,6 @@ package ClasesAbstractas;
 
 public abstract class Animal extends SerVivo{
 
-   
+   //No ocupamos poner nada por que no ocupamos instanciar nada ni sobre escribir nada en este punto, solo que sirva como parte de la herencia
     
 }
