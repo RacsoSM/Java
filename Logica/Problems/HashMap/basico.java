b4d224fbfac8aac2f665575b8a1039dc554afa76
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-29 23:17:27 -0700

    Main con HashMap declarado

--- Logica/Problems/HashMap/basico.java
+++ Logica/Problems/HashMap/basico.java
@@ -6,9 +6,11 @@ public class basico {
     /*Un HashMap es un tipo de Map que se basa en dar una clave (key) y un valor (value) a algo
     Sintaxis: 
     HashMap<String,Integer> hash = new HashMap <> ();
-    Le tenemos que dar como parametros entre los primeros diamantes <> primero, el tipo de dato que será la key y luego el valor.
+    Le tenemos que dar como parametros entre los primeros diamantes <> primero, el tipo de dato que será la key y luego el valor (Solo clases primitivas). 
      * 
      */
-  
+    public static void main(String[] args) {
+        HashMap<String,Integer> hash = new HashMap <> (); //HashMap de key String y value Integer de nombre hash
+    }
     
 }
