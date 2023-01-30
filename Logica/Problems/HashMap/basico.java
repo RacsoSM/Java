548478836140852d0f4744ee281fb260569b9111
RacsoSM <120708100+RacsoSM@users.noreply.github.com>
2023-01-29 23:14:49 -0700

    Documentacion de sintaxis HashMap

--- Logica/Problems/HashMap/basico.java
+++ Logica/Problems/HashMap/basico.java
@@ -1,3 +1,10 @@
 public class basico {
+
+    /*Un HashMap es un tipo de Map que se basa en dar una clave (key) y un valor (value) a algo
+    Sintaxis: 
+    HashMap<String,Integer> hash = new HashMap <> ();
+    Le tenemos que dar como parametros entre los primeros diamantes <> primero, el tipo de dato que será la key y luego el valor.
+     * 
+     */
     
 }
